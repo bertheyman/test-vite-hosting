5bb071934bda0b0ed8e1a2c1e22e4181a03f8e6c
Bert Heyman <bert.heyman@hotmail.com>
2026-09-10 14:54:03 +0200

    Typo

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-    base: command === 'build' ? '/vite-test-hosting/' : '/',
+    base: command === 'build' ? '/test-vite-hosting/' : '/',
 }))
